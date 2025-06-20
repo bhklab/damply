b61cbf4fc4095e4bc05334876f8d3195400d5812
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-06-20 11:57:50 -0400

    feat: ensure working audit

--- src/damply/cli/__main__.py
+++ src/damply/cli/__main__.py
@@ -1,6 +1,7 @@
 import click
 
 from damply import __version__
+from damply.cli.audit import audit
 from damply.cli.groups_table import groups_table
 from damply.cli.project import project
 from damply.cli.whose import whose
@@ -23,6 +24,7 @@ def cli() -> None:
 	pass
 
 
-cli.add_command(project)
 cli.add_command(groups_table)
 cli.add_command(whose)
+cli.add_command(project)
+cli.add_command(audit)
